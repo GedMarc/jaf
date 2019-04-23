da7425b3ee97a305a8d201e0c9b1e8b6f734db29
GedMarc <ged_marc@hotmail.com>
2019-04-23 10:01:14 +0200

    javax.annotation JPMS

---
+++ src/jre11/java/module-info.java
@@ -0,0 +1,10 @@
+module java.activation {
+	requires static java.datatransfer;
+	requires static java.desktop;
+	requires java.logging;
+
+	exports javax.activation;
+
+/*	exports com.sun.activation.registries;
+	exports com.sun.activation.viewers;*/
+}
