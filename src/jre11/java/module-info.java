01dce90760c6a156bec60b357387ab1fd431d002
GedMarc <ged_marc@hotmail.com>
2019-05-19 02:29:45 +0200

    Strictly name cache API, sql server driver Allow java.activation and jakarta.activation modules in one (great going thanks guys ';/') Leave it to the project owners to do the tests (some take forever)

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,10 +1,8 @@
 module jakarta.activation {
+
 	requires static java.datatransfer;
 	requires static java.desktop;
+
 	requires java.logging;
-
 	exports javax.activation;
-
-/*	exports com.sun.activation.registries;
-	exports com.sun.activation.viewers;*/
 }
